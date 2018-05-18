fc0685a97a9ca7803f0bb8e4979f8458c5f21698
Aizhan Osmonova <osmonova86@gmail.com>
2018-05-18 13:29:52 -0400

    feat: Implement the Enemy class
    - The Enemy function, which initiates the Enemy by:
    - Load the image by setting this.sprite to the bug image
    - Set random row
    - Set initial location
    - Set random speed

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+  let randomY = Math.floor(Math.random() * Math.floor(3))+1;
+  this.speed = 50 + Math.floor(Math.random() * Math.floor(300));
+  this.x = 0;
+  this.y = 73*randomY;
+  // The image/sprite for enemies, this uses
+  // a helper to easily load images
+  this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
@@ -29,8 +31,10 @@ Enemy.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+var bug1 = new Enemy();
+var bug2 = new Enemy();
+var bug3 = new Enemy();
+var allEnemies = [bug1, bug2, bug3];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
